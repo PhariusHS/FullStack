6cd5553957fff39ac4fa719ae0a4e2b10dc9e1a4
Vaabrate <93230295+Vaabrate@users.noreply.github.com>
2023-06-21 11:14:13 -0300

    comprobaciones
    funcionamiento de las comprobaciones de ganadores, casillas ocupadas y perdida de turnos

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ var matriz = [  [11, 12, 14, 15, 16, 18, 20],
                 [80, 81, 84, 88, 90, 96, 99], 
                 [100,108,110,120,121,132,144]];
 
-
+ 
                 for (let i = 1; i <= 12; i++) {
                     const boton = document.getElementById(`boton${i}`);
                     boton.addEventListener("click", function() {
                       if (miNum1 == 0) {
                         miNum1 = i;
-
+                        
                         if (miNum1 == miNum2) {
                           resu = miNum2 * miNum1;
                           boton.style.backgroundImage = "linear-gradient(to right, #42ab49, #ec5853)";
@@ -43,23 +43,25 @@ var matriz = [  [11, 12, 14, 15, 16, 18, 20],
                           miNum1 = 0;
                           resu = miNum2 * miNum1;
                           boton.style.backgroundImage = "linear-gradient(to right, #42ab49, #42ab49)";
-
+                         
                         } else {
                           miNum1 = 0;
                           resu = miNum2 * miNum1;
                           boton.style.backgroundImage = "linear-gradient(to right, #ffd992, #ffd992)";
-
+                         
                         }
                       } else if (miNum2 == i) {
                         miNum2 = 0;
                         resu = miNum2 * miNum1;
                         boton.style.backgroundImage = "linear-gradient(to right, #ffd992, #ffd992)";
-
+                        
                       }
                      refrescar(resu); 
                     });
                   }
                   
+
+
                   var contador = 0;
                   function refrescar(resu,contador){
                       
@@ -118,7 +120,22 @@ var matriz = [  [11, 12, 14, 15, 16, 18, 20],
 
 
                         }
-                       
+                        //comprueba casilla inexsitente
+                        if (resu!= 0 && resu!= 11 && resu!= 12 && resu!= 14 && resu!= 15 && resu!= 16 && resu!= 18 && resu!= 20
+                          && resu!= 21 && resu!= 22 && resu!= 24 && resu!= 25 && resu!= 27 && resu!= 28 && resu!= 30
+                          && resu!= 32 && resu!= 33 && resu!= 35 && resu!= 36 && resu!= 40 && resu!= 42 && resu!= 44
+                          && resu!= 45 && resu!= 48 && resu!= 49 && resu!= 50 && resu!= 54 && resu!= 55 && resu!= 56
+                          && resu!= 60 && resu!= 63 && resu!= 64 && resu!= 66 && resu!= 70 && resu!= 72 && resu!= 77
+                          && resu!= 80 && resu!= 81 && resu!= 84 && resu!= 88 && resu!= 90 && resu!= 96 && resu!= 99
+                          && resu!= 100 && resu!= 108 && resu!= 110 && resu!= 120 && resu!= 121 && resu!= 132 && resu!= 144 ){
+
+                          alert("Casilla inexistente, perdiste el turno")
+                          document.getElementById("caja").innerHTML = "JUEGA VERDE (ROJO PERDIO TURNO)";
+                          document.getElementById("caja").style.fontSize = "2vw";
+                          contador++;
+                          contT= contT+2;
+                          resu=0;
+                        }
                          
                   
                       
@@ -143,7 +160,22 @@ var matriz = [  [11, 12, 14, 15, 16, 18, 20],
                           }
 
                   }
-                  
+                  //comprueba casilla inexsitente
+                  if ( resu!= 0 && resu!= 11 && resu!= 12 && resu!= 14 && resu!= 15 && resu!= 16 && resu!= 18 && resu!= 20
+                    && resu!= 21 && resu!= 22 && resu!= 24 && resu!= 25 && resu!= 27 && resu!= 28 && resu!= 30
+                    && resu!= 32 && resu!= 33 && resu!= 35 && resu!= 36 && resu!= 40 && resu!= 42 && resu!= 44
+                    && resu!= 45 && resu!= 48 && resu!= 49 && resu!= 50 && resu!= 54 && resu!= 55 && resu!= 56
+                    && resu!= 60 && resu!= 63 && resu!= 64 && resu!= 66 && resu!= 70 && resu!= 72 && resu!= 77
+                    && resu!= 80 && resu!= 81 && resu!= 84 && resu!= 88 && resu!= 90 && resu!= 96 && resu!= 99
+                    && resu!= 100 && resu!= 108 && resu!= 110 && resu!= 120 && resu!= 121 && resu!= 132 && resu!= 144 ){
+
+                    alert("Casilla inexistente, perdiste el turno")
+                    document.getElementById("caja").innerHTML = " JUEGA ROJO (VERDE PERDIO TURNO)";
+                    document.getElementById("caja").style.fontSize = "2vw";
+                    contador++;
+                    contT= contT+2;
+                    resu=0;
+                  }
                   
                  
                       //Compara todas las filas y 3 columnas menos
@@ -194,3 +226,18 @@ var matriz = [  [11, 12, 14, 15, 16, 18, 20],
                       return contador;
                   }
                   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
